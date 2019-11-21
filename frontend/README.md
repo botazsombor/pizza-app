43cb3a87dfa3ce066f6b853195a2256b8a933af8
Zsombor Bota <zsomborbota@kuka-robotics.hu>
2019-11-21 07:44:59 +0100

    app base

--- frontend/README.md
+++ frontend/README.md
@@ -1,6 +1,6 @@
-# PizzaFrontend
+# IssueTrackerClient
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.6.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.9.
 
 ## Development server
 
---
+++ frontend/src/app/issue.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Issue } from "./issue";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic dXNlcjpwYXNzd29yZA==', // user/password
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IssueService {
+  
+  private issueUrl: string = 'http://localhost:8080/issues';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getIssues(): Promise<Issue[]> {
+    return this.http.get<Issue[]>(`${this.issueUrl}`, httpOptions).toPromise();
+  }
+
+  getIssue(id: number): Promise<Issue> {
+    return this.http.get<Issue>(`${this.issueUrl}/${id}`, httpOptions).toPromise();
+  }
+
+  createIssue(issue: Issue): Promise<Issue> {
+    return this.http.post<Issue>(`${this.issueUrl}`, issue, httpOptions).toPromise();
+  }
+
+  updateIssue(issue: Issue): Promise<Issue> {
+    return this.http.put<Issue>(`${this.issueUrl}/${issue.id}`, issue, httpOptions).toPromise();
+  }
+
+  deleteIssue(id): Promise<Issue> {
+    return this.http.delete<Issue>(`${this.issueUrl}/${id}`, httpOptions).toPromise();
+  }
+}
