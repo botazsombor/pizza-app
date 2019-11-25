6d9bec41f3c7c0755f37101e7af509cdf51cb1b6
Zsombor Bota <zsomborbota@kuka-robotics.hu>
2019-11-25 09:28:07 +0100

    generated components

--- frontend/src/app/profile/profile.component.spec.ts
+++ frontend/src/app/profile/profile.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { StatusFilterComponent } from './status-filter.component';
+import { ProfileComponent } from './profile.component';
 
-describe('StatusFilterComponent', () => {
-  let component: StatusFilterComponent;
-  let fixture: ComponentFixture<StatusFilterComponent>;
+describe('ProfileComponent', () => {
+  let component: ProfileComponent;
+  let fixture: ComponentFixture<ProfileComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StatusFilterComponent ]
+      declarations: [ ProfileComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(StatusFilterComponent);
+    fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- frontend/src/app/routing/routing.module.ts
+++ frontend/src/app/routing/routing.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { UserListComponent } from "../user-list/user-list.component";
 import { UserFormComponent } from '../user-form/user-form.component';
 import { UserDetailComponent } from '../user-detail/user-detail.component';
 import { PizzaListComponent } from '../pizza-list/pizza-list.component';
+import { ProfileComponent } from '../profile/profile.component';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
+import { CartComponent } from '../cart/cart.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/users',
+    redirectTo: 'pizzas',
     pathMatch: 'full'
-  },
-  {
-    path: 'users',
-    component: UserListComponent
-  },
-  {
-    path: 'users/add',
-    component: UserFormComponent
   },
   {
     path: 'users/:id',
@@ -28,6 +22,26 @@ const routes: Routes = [
     path: 'pizzas',
     component: PizzaListComponent
   },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  }
 ];
 
 @NgModule({
